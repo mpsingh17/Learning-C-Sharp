38a927649751a6d7494dbfca01984c73eb971e14
Manpreet <mpsingh17@outlook.co.nz>
2023-08-05 12:13:46 +1200

    Code refactoring to make it more readable.

--- C# Basics/Program.cs
+++ C# Basics/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Let's pay the guess number game.");
+﻿// Game start greeting messages.
+Console.WriteLine("Let's pay the guess number game.");
 Console.WriteLine("I'm thinking of a number between 0 - 30.");
 Console.WriteLine("Let's see how many guesses you'll take to guess the right number.");
 Console.WriteLine("If you want to quit, type -1.");
@@ -10,18 +11,19 @@ var randomNumber = new Random().Next(30);
 var guessCounter = 0;
 
 // flag variable to enable program to keep asking for the input from the user.
-var success = false;
+var keepGoing = true;
 
-while (!success)
+while (keepGoing)
 {
     var enteredValue = Console.ReadLine();
 
-    success = Int32.TryParse(enteredValue, out var parsedEnteredValue);
+    keepGoing = Int32.TryParse(enteredValue, out var parsedEnteredValue);
 
     // if user input anything other than a natural number, show message and skip the iteration.
-    if(!success)
+    if(!keepGoing)
     {
-        Console.WriteLine("Please enter a natural number between 0 to 30");
+        Console.WriteLine("Please enter a natural number between 0 and 30");
+        keepGoing = true;
         continue;
     }
 
@@ -31,38 +33,31 @@ while (!success)
         Console.WriteLine($"You did your best. Better luck next time. You made {guessCounter} guesses");
         break;
     }
-
-    if (parsedEnteredValue < 0 | parsedEnteredValue > 30)
-    {
-        Console.WriteLine("Please enter a value between 0 and 30");
-        success = false; // set success to false so loop can continue to run.
-        continue;
-    }
-
+    
     // User made a guess so increment the guess counter.
     guessCounter++;
 
+    // If the parsedEnterValue is not between the 0 and 30.
+    if (parsedEnteredValue < 0 | parsedEnteredValue > 30)
+    {
+        Console.WriteLine("Please enter a value between 0 and 30");
+        keepGoing = true;
+        continue;
+    }
+    
     // User won.
     if (parsedEnteredValue == randomNumber)
     {
         Console.WriteLine($"You Win! You took {guessCounter} guesses to win.");
         break;
     }
-    // User input is greater than guessed number.
-    else if (parsedEnteredValue > randomNumber)
-    {
-        Console.WriteLine("Your guess is greater.");
 
-    }
-    // user input is lower than guessed number.
-    else
-    {
-        Console.WriteLine("Your guess is lower.");
-    }
+    Console.WriteLine(
+        "You guessed {0} than random number.",
+        parsedEnteredValue > randomNumber ? "greater" : "lower"
+    );
 
-    // User didn't won so set success to false to keep the loop going.
-    success = false;
-
+    keepGoing = true;
 }
 
 
